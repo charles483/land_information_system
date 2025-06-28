58010e209424060309b00e72a496100439ad3939
Charles Churu <c4708911@gmail.com>
2025-06-28 22:12:41 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -9,45 +9,29 @@ import LandRecords from './components/LandRecords';
 import UserProfile from './components/UserProfile';
 import Contact from './pages/Contact';
 import ViewMore from './pages/ViewMore';
-import Login from './auth/Login';
-import Register from './auth/Register';
 import './App.css';
 
 function App() {
-  const [loggedInUser, setLoggedInUser] = useState(null);
-
   return (
     <Router>
       <div className="app">
-        {loggedInUser && <Navbar />}
-        
+        <Navbar />
+
         <main>
           <div className="page-container">
             <Routes>
-              {!loggedInUser ? (
-                <>
-                  <Route path="/*" element={<Navigate to="/login" />} />
-                  <Route path="/login" element={<Login setUser={setLoggedInUser} />} />
-                  <Route path="/register" element={<Register setUser={setLoggedInUser} />} />
-                </>
-              ) : (
-                <>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/parcel-search" element={<ParcelSearch />} />
-                  <Route path="/map-view" element={<MapView />} />
-                  <Route path="/land-records" element={<LandRecords />} />
-                  <Route path="/user" element={<UserProfile user={loggedInUser} />} />
-                  <Route path="/contact" element={<Contact />} />
-                  <Route path="/view-more" element={<ViewMore />} />
-                  <Route path="/login" element={<Navigate to="/" />} />
-                  <Route path="/register" element={<Navigate to="/" />} />
-                </>
-              )}
+              <Route path="/" element={<Home />} />
+              <Route path="/parcel-search" element={<ParcelSearch />} />
+              <Route path="/map-view" element={<MapView />} />
+              <Route path="/land-records" element={<LandRecords />} />
+              <Route path="/user" element={<UserProfile />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/view-more" element={<ViewMore />} />
             </Routes>
           </div>
         </main>
 
-        {loggedInUser && <Footer />}
+        <Footer />
       </div>
     </Router>
   );
